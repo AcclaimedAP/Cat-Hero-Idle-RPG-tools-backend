3fc99edd277792ce98be510087beaba5a32a1345
AcclaimedAP <alexander.w.pressfelt@gmail.com>
2024-02-23 23:44:43 +0100

    Change parameter to str

--- builds/urls.py
+++ builds/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("<int:build_id>/", views.show, name="show"),
+    path("<str:build_id>/", views.show, name="show"),
     path("new/", views.create, name="create"),
 ]
