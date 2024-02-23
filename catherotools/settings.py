51d939e021a91a24157f2edff433f93c54c5bb05
AcclaimedAP <alexander.w.pressfelt@gmail.com>
2024-02-24 00:23:47 +0100

    Update settings.py

--- catherotools/settings.py
+++ catherotools/settings.py
@@ -27,9 +27,9 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ["178.128.198.164", "api.cathero.tools"]
+ALLOWED_HOSTS = ["178.128.198.164", "api.cathero.tools", "cathero.tools"]
 CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS").split(",")
-CORS_ALLOWED_ORIGINS = ["http://localhost:5173", "https://api.cathero.tools", "https://cathero.tools", "http://178.128.198.164", "https://acclaimedap.github.io"]
+CORS_ALLOWED_ORIGINS = ["http://localhost:5173", "https://api.cathero.tools", "https://cathero.tools", "cathero.tools", "http://178.128.198.164", "https://acclaimedap.github.io"]
 # Application definition
 
 INSTALLED_APPS = [
