c7538967d870f555b8278c05a1d49eda513194ca
AcclaimedAP <alexander.w.pressfelt@gmail.com>
2024-02-24 00:44:04 +0100

    Fix issue of overlapping urls

--- builds/urls.py
+++ builds/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("<str:build_id>/", views.show, name="show"),
+    path("list/<str:build_id>/", views.show, name="show"),
     path("new/", views.create, name="create"),
 ]
