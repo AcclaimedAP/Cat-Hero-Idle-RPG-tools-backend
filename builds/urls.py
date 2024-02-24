27aff2e550ff8ec2e4c79e8e805e2697678bc26b
Mattéo Christophe <matteo.christophe@opquast.com>
2024-02-24 15:45:01 +0100

    fixed builds url

--- builds/urls.py
+++ builds/urls.py
@@ -3,5 +3,5 @@ from .views import BuildView
 
 urlpatterns = [
     path("<str:build_id>/", BuildView.as_view(), name="show"),
-    path("new/", BuildView.as_view(), name="create"),
+    path("", BuildView.as_view(), name="create"),
 ]
