8a0bd289448e46bea811bbe5c385bc88f7d15603
Mattéo <60100878+Matteo-Ynov@users.noreply.github.com>
2024-02-24 15:00:40 +0100

    Feat/pre commit (#2)
    * added base redirection and updated gitignore
    
    * added pre commit + flake 8 and changed views to be class based
    
    * Update settings.py
    
    ---------
    
    Co-authored-by: Mattéo Christophe <matteo.christophe@opquast.com>
    Co-authored-by: Alexander Pressfelt <alexander.w.pressfelt@gmail.com>

--- builds/views.py
+++ builds/views.py
@@ -1,28 +1,26 @@
-from django.shortcuts import render
 from django.http import JsonResponse
-
+from django.views import View
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
 from .models import BuildModel
 
 
+@method_decorator(csrf_exempt, name='dispatch')
+class BuildView(View):
+    def get(self, request, build_id):
+        if build_id:
+            try:
+                build = BuildModel.objects.get(id=build_id)
+                return JsonResponse({'build': build.build_string})
+            except BuildModel.DoesNotExist:
+                return JsonResponse({'error': 'No build found'})
+        else:
+            return JsonResponse({'error': 'No build id provided'})
 
-
-def show(request, build_id):
-    if build_id:
-        try:
-          build = BuildModel.objects.get(id=build_id)
-          return JsonResponse({'build': build.build_string})
-        except BuildModel.DoesNotExist:
-          return JsonResponse({'error': 'No build found'})
-    else:
-        return JsonResponse({'error': 'No build id provided'})
-    
-
-
-def create(request):
-    if request.method == 'POST':
+    def post(self, request):
         build_string = request.body.decode('utf-8')
         if build_string == "":
-          return JsonResponse({'error': 'Invalid build string'})
+            return JsonResponse({'error': 'Invalid build string'})
         if build_string:
             build = BuildModel(build_string=build_string)
             build.save()
@@ -30,5 +28,3 @@ def create(request):
             return JsonResponse({'build_id': build.id}, status=201)
         else:
             return JsonResponse({'error': 'No build string provided'})
-    else:
-        return JsonResponse({'error': 'Invalid request method'})
