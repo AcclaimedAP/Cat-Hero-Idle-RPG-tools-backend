c6925f9c71ee9bbc8a513be705c958eb2134cb20
Mattéo Christophe <matteo.christophe@opquast.com>
2024-02-24 16:48:26 +0100

    tweaked build create view to send existing build model if needed

--- builds/views.py
+++ builds/views.py
@@ -3,18 +3,20 @@ from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 from .models import BuildModel
+from django.utils import timezone
 
 
-@method_decorator(csrf_exempt, name='dispatch')
 class BuildCreateView(View):
     def post(self, request):
-        build_string = request.body.decode('utf-8')
+        build_string = request.body.decode('utf-8').strip()
         if not build_string:
             return JsonResponse({'error': 'Invalid build string'}, status=400)
 
-        build = BuildModel(build_string=build_string)
-        build.save()
-        return JsonResponse({'build_id': build.id}, status=201)
+        build, created = BuildModel.objects.get_or_create(build_string=build_string)
+        if created:
+            build.last_accessed = timezone.now()
+        status_code = 201 if created else 200  # 201 Created if new object was created, else 200 OK
+        return JsonResponse({'build_id': build.id}, status=status_code)
 
 
 @method_decorator(csrf_exempt, name='dispatch')
