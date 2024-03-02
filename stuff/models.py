5b6a2e7f2036ae48face92f62cb301e29e2c20de
Mattéo <60100878+Matteo-Ynov@users.noreply.github.com>
2024-03-02 20:14:26 +0100

    Poc companions model (#11)
    * added base redirection and updated gitignore
    
    * created basic data models
    
    * typo
    
    * added base skills and comp
    
    ---------
    
    Co-authored-by: Mattéo Christophe <matteo.christophe@opquast.com>

---
+++ stuff/models.py
@@ -0,0 +1,90 @@
+from django.db import models
+from django.contrib.postgres.fields import ArrayField
+
+
+class Companion(models.Model):
+    RARITY_CHOICES = [
+        ('common', 'Common'),
+        ('uncommon', 'Uncommon'),
+        ('rare', 'Rare'),
+        ('epic', 'Epic'),
+        ('legendary', 'Legendary'),
+        ('mythic', 'Mythic'),
+    ]
+
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
+    rarity = models.CharField(max_length=10, choices=RARITY_CHOICES)
+    base_mp = models.IntegerField()
+    types = models.ManyToManyField('Type')
+
+
+class Skill(models.Model):
+    RARITY_CHOICES = [
+        ('common', 'Common'),
+        ('uncommon', 'Uncommon'),
+        ('rare', 'Rare'),
+        ('epic', 'Epic'),
+        ('legendary', 'Legendary'),
+        ('mythic', 'Mythic'),
+    ]
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
+    rarity = models.CharField(max_length=10, choices=RARITY_CHOICES)
+    description = models.TextField(max_length=500)
+    types = models.ManyToManyField('Type')
+    cooldown = models.FloatField()
+
+
+class Type(models.Model):
+    # ['food', 'plant', 'animal', 'machine', 'dessert', 'chicken', 'hamburger', 'cloud', 'pirate', 'wolf', 'cat', 'magic', 'dragon', 'shark', 'small', 'medium', 'large']
+    slug = models.CharField(max_length=50, unique=True)
+
+    def __str__(self) -> str:
+        return self.slug
+
+
+class RuneType(models.Model):
+    RUNE_TYPES = [
+        'increase_crit_percent',  # ["percentage"],
+        'atk_boost',  # ["percentage"],
+        'fish_dmg_boost',  # ["percentage"],
+        'boss_dmg',  # ["percentage"],
+        'crit_dmg_boost',  # ["percentage"],
+        'triple_shot',  # ["percentage"],
+        'double_shot',  # ["percentage"],
+        'increase_all_dmg_per_companion_type',  # ["percentage", "companion_type"],
+        'increase_skill_type_dmg_per_companion_type',  # ["percentage", "type"],
+        'reduce_mp_for_companion_type_and_rarity',  # ["mp_reduction", "type", "rarity"],
+        'increase_skill_rarity_dmg_per_companion_rarity',  # ["percentage", "skill_rarity", "companion_rarity"],
+        'increase_final_dmg_after_skill',  # ["percentage", "duration", "skill_type"],
+        'increase_deal_ratio_per_companion_type',  # ["percentage", "type"],
+        'increase_crit_dmg_boost_per_companion_type',  # ["percentage", "type"],
+        'increase_crit_dmg_per_companion_type',  # ["percentage", "type"],
+        'increase_max_mp',  # ["mp_increase"]
+    ]
+    name = models.CharField(max_length=80, unique=True)
+    values_required = ArrayField(models.CharField(max_length=100), default=list)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Rune(models.Model):
+    RARITY_CHOICES = [
+        ('uncommon', 'Uncommon'),
+        ('rare', 'Rare'),
+        ('epic', 'Epic'),
+        ('legendary', 'Legendary'),
+        ('mythic', 'Mythic'),
+    ]
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=50)
+    rarity = models.CharField(max_length=10, choices=RARITY_CHOICES)
+    description = models.TextField(max_length=200)
+    type = models.ForeignKey(RuneType, on_delete=models.SET_NULL, related_name='runes', null=True)
+    values = ArrayField(models.CharField(max_length=100), default=list)
+
+
+class Runezggerge(models.Model):
+    id = models.IntegerField(primary_key=True)
